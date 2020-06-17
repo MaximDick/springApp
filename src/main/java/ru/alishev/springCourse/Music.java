db30e10ae05020a721a0b20c429b9828e79c11ed
Maxim Dick <maxim1994barca@gmail.com>
2020-06-17 15:42:09 +0300

    lesson 4:Инверсия управления. Inversion of Control (IoC).

---
+++ src/main/java/ru/alishev/springCourse/Music.java
@@ -0,0 +1,5 @@
+package ru.alishev.springCourse;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/ru/alishev/springCourse/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.alishev.springCourse;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wing of change";
+    }
+}
